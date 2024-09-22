f65219ffac8b0f898a44f465b8e2a5dbfd5a6804
Johnny Street <johnnystreet@gmail.com>
2024-09-22 15:56:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='stable-audio-tools',
     version='0.0.16',
-    url='https://github.com/Stability-AI/stable-audio-tools.git',
+    url='https://github.com/monofy-org/stable-audio-tools.git',
     author='Stability AI',
     description='Training and inference tools for generative audio models from Stability AI',
     packages=find_packages(),  
